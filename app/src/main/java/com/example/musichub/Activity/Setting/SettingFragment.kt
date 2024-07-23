79c5fdcc64bb9105313e184e17af4b7f5fab5c4e
tmddn7475 <junseungu74@naver.com>
2024-07-23 18:32:41 +0900

    Initial commit

--- app/src/main/java/com/example/musichub/Activity/Setting/SettingFragment.kt
+++ app/src/main/java/com/example/musichub/Activity/Setting/SettingFragment.kt
@@ -1,12 +1,19 @@
 package com.example.musichub.Activity.Setting
 
+import android.app.AlertDialog
 import android.content.Intent
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
+import androidx.core.app.ActivityCompat
 import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
 import com.example.musichub.Activity.AccountEditActivity
+import com.example.musichub.Activity.LoginActivity
 import com.example.musichub.Activity.UploadActivity
+import com.example.musichub.Command
 import com.example.musichub.R
+import com.google.firebase.auth.FirebaseAuth
 
 class SettingFragment: PreferenceFragmentCompat() {
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
@@ -31,6 +38,21 @@ class SettingFragment: PreferenceFragmentCompat() {
     }
 
     private fun logOut(){
-
+        val alert_ex: AlertDialog.Builder = AlertDialog.Builder(requireContext())
+        alert_ex.setMessage("로그아웃하시겠습니까?")
+        alert_ex.setNegativeButton("네") { _, _ ->
+            Command().deleteAll(requireContext())
+            FirebaseAuth.getInstance().signOut()
+            val intent = Intent(requireContext(), LoginActivity::class.java)
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK)
+            startActivity(intent)
+            ActivityCompat.finishAffinity(requireActivity())
+        }
+        alert_ex.setPositiveButton("아니요") { dialog, _ ->
+            dialog.dismiss()
+        }
+        val alert = alert_ex.create()
+        alert.window!!.setBackgroundDrawable(ColorDrawable(Color.DKGRAY))
+        alert.show()
     }
 }
