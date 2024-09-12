8e6de9e49dd7968919ee79d490227879aa720625
tmddn7475 <junseungu74@naver.com>
2024-09-13 08:38:31 +0900

    commit

--- app/src/main/java/com/example/musichub/Adapter/Recycler/PlayListAdapter.kt
+++ app/src/main/java/com/example/musichub/Adapter/Recycler/PlayListAdapter.kt
@@ -7,6 +7,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.example.musichub.Data.MusicData
@@ -29,6 +30,8 @@ class PlayListAdapter(val list: ArrayList<MusicData>, var playListListener: Play
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         if (position == selected) {
             holder.songName.setTextColor(Color.parseColor("#00B3EF"))
+        } else {
+            holder.songName.setTextColor(ContextCompat.getColor(holder.itemView.context, R.color.text))
         }
 
         holder.songName.text = list[position].songName
@@ -41,6 +44,7 @@ class PlayListAdapter(val list: ArrayList<MusicData>, var playListListener: Play
             playListListener.sendMusic(list[position].songUrl)
         }
 
+        // EtcFragment
         holder.songEtc.setOnClickListener {
             playListListener.sendEtc(list[position].songUrl)
         }
